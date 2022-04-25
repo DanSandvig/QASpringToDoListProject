b7da70cfd60cc290cd412f5e2b7b8994df6393b0
DanSandvig <dspublicaddress@protonmail.com>
2022-04-25 14:16:52 +0100

    Initial commit

---
+++ src/main/java/com/bae/qaspringtodolistproject/QaSpringToDoListProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bae.qaspringtodolistproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaSpringToDoListProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaSpringToDoListProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bae/qaspringtodolistproject/QaSpringToDoListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae.qaspringtodolistproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaSpringToDoListProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
