2023dbb7bc3a51e8a4c2fbbcd766e210a7ea20af
DanSandvig <dspublicaddress@protonmail.com>
2022-04-26 09:59:17 +0100

    Added annotation and inheritance for functioning repo

--- src/main/java/com/bae/qaspringtodolistproject/repo/ToDoListEntryRepo.java
+++ src/main/java/com/bae/qaspringtodolistproject/repo/ToDoListEntryRepo.java
@@ -1,5 +1,11 @@
 package com.bae.qaspringtodolistproject.repo;
 
-public interface ToDoListEntryRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bae.qaspringtodolistproject.domain.ToDoListEntry;
+
+@Repository
+public interface ToDoListEntryRepo extends JpaRepository<ToDoListEntry, Long> {
 
 }
