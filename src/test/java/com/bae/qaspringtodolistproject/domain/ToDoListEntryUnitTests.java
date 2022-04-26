70fe47cc4d8bf65055d850faa06262b8923c5014
DanSandvig <dspublicaddress@protonmail.com>
2022-04-26 14:28:03 +0100

    Unit tests for hashcode, equals and constructors, had to modify entity

--- src/test/java/com/bae/qaspringtodolistproject/domain/ToDoListEntryUnitTests.java
+++ src/test/java/com/bae/qaspringtodolistproject/domain/ToDoListEntryUnitTests.java
@@ -1,5 +1,53 @@
 package com.bae.qaspringtodolistproject.domain;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
 public class ToDoListEntryUnitTests {
 
+	//Tests equals and hashCode in a single method (required them to not use id?)
+	@Test
+	public void testEquals() {
+		EqualsVerifier.forClass(ToDoListEntry.class).usingGetClass().verify();
+	}
+	
+	@Test
+	public void testConstructorWithId() {
+		ToDoListEntry newToDo = new ToDoListEntry(1L, 1, "Do the thing!",
+								"There are many things. Do one of them.", false);
+		
+		assertNotNull(newToDo.getId());
+		assertNotNull(newToDo.getPriority());
+		assertNotNull(newToDo.getTitle());
+		assertNotNull(newToDo.getDescription());
+		assertNotNull(newToDo.isComplete());
+		
+		assertEquals(1L, newToDo.getId());
+		assertEquals(1, newToDo.getPriority());
+		assertEquals("Do the thing!", newToDo.getTitle());
+		assertEquals("There are many things. Do one of them.", newToDo.getDescription());
+		assertEquals(false, newToDo.isComplete());
+	}
+	
+	@Test
+	public void testConstructorWithoutId() {
+		ToDoListEntry newToDo = new ToDoListEntry(1, "Do the thing!",
+								"There are many things. Do one of them.", false);
+		
+		assertNotNull(newToDo.getId());
+		assertNotNull(newToDo.getPriority());
+		assertNotNull(newToDo.getTitle());
+		assertNotNull(newToDo.getDescription());
+		assertNotNull(newToDo.isComplete());
+		
+		assertEquals(0L, newToDo.getId());
+		assertEquals(1, newToDo.getPriority());
+		assertEquals("Do the thing!", newToDo.getTitle());
+		assertEquals("There are many things. Do one of them.", newToDo.getDescription());
+		assertEquals(false, newToDo.isComplete());
+	}
+	
 }
