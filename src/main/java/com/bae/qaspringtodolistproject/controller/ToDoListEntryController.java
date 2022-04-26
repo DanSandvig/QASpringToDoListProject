dd8d3c1d95d4de5c3f0768423680d1c38484897d
DanSandvig <dspublicaddress@protonmail.com>
2022-04-26 11:47:59 +0100

    Added annotations, service and constructor to controller

--- src/main/java/com/bae/qaspringtodolistproject/controller/ToDoListEntryController.java
+++ src/main/java/com/bae/qaspringtodolistproject/controller/ToDoListEntryController.java
@@ -1,5 +1,21 @@
 package com.bae.qaspringtodolistproject.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bae.qaspringtodolistproject.service.ToDoListEntryService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/todolist")
 public class ToDoListEntryController {
 
+	private ToDoListEntryService toDoListService;
+
+	public ToDoListEntryController(ToDoListEntryService toDoListService) {
+		super();
+		this.toDoListService = toDoListService;
+	}
+	
 }
