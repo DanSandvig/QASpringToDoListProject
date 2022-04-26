5f0ef606c0f629551819acb22a2dbee0375ac52e
DanSandvig <dspublicaddress@protonmail.com>
2022-04-26 14:45:06 +0100

    Created unit tests for all setters and toString

--- src/test/java/com/bae/qaspringtodolistproject/domain/ToDoListEntryUnitTests.java
+++ src/test/java/com/bae/qaspringtodolistproject/domain/ToDoListEntryUnitTests.java
@@ -50,4 +50,39 @@ public class ToDoListEntryUnitTests {
 		assertEquals(false, newToDo.isComplete());
 	}
 	
+	@Test
+	public void testSetters() {
+		ToDoListEntry newToDo = new ToDoListEntry();
+		
+		newToDo.setId(1L);
+		newToDo.setPriority(1);
+		newToDo.setTitle("Do the thing!");
+		newToDo.setDescription("There are many things. Do one of them.");
+		newToDo.setComplete(false);
+		
+		assertNotNull(newToDo.getId());
+		assertNotNull(newToDo.getPriority());
+		assertNotNull(newToDo.getTitle());
+		assertNotNull(newToDo.getDescription());
+		assertNotNull(newToDo.isComplete());
+		
+		assertEquals(1L, newToDo.getId());
+		assertEquals(1, newToDo.getPriority());
+		assertEquals("Do the thing!", newToDo.getTitle());
+		assertEquals("There are many things. Do one of them.", newToDo.getDescription());
+		assertEquals(false, newToDo.isComplete());
+	}
+	
+	@Test
+	public void testToString() {
+		ToDoListEntry newToDo = new ToDoListEntry(1, "Do the thing!",
+				"There are many things. Do one of them.", false);
+		
+		assertNotNull(newToDo.toString());
+		
+		assertEquals("ToDoListEntry [id=0, priority=1, title=Do the thing!,"
+				+ " description=There are many things. Do one of them., complete=false]",
+				newToDo.toString());
+	}
+	
 }
