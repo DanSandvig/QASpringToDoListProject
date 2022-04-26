a43f533678bec4646416892fdd73323ffc1a0b6f
DanSandvig <dspublicaddress@protonmail.com>
2022-04-26 09:37:22 +0100

    Added five properties to ToDoListEntry

--- src/main/java/com/bae/qaspringtodolistproject/domain/ToDoListEntry.java
+++ src/main/java/com/bae/qaspringtodolistproject/domain/ToDoListEntry.java
@@ -1,5 +1,28 @@
 package com.bae.qaspringtodolistproject.domain;
 
-public class ToDoListEntry {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class ToDoListEntry {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false)
+	private int priority;
+	
+	@Column(nullable = false)
+	private String title;
+	
+	@Column
+	private String description;
+	
+	@Column(nullable = false)
+	private boolean complete;
 }
