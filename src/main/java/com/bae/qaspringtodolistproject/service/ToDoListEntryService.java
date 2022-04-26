e9b30a04efa9bc7cad3a678293bad70bf03b63d5
DanSandvig <dspublicaddress@protonmail.com>
2022-04-26 10:37:17 +0100

    Added repo and constructor to service

--- src/main/java/com/bae/qaspringtodolistproject/service/ToDoListEntryService.java
+++ src/main/java/com/bae/qaspringtodolistproject/service/ToDoListEntryService.java
@@ -1,5 +1,18 @@
 package com.bae.qaspringtodolistproject.service;
 
-public class ToDoListEntryService {
+import org.springframework.stereotype.Service;
 
+import com.bae.qaspringtodolistproject.repo.ToDoListEntryRepo;
+
+@Service
+public class ToDoListEntryService {
+	
+	private ToDoListEntryRepo toDoListEntryRepo;
+
+	public ToDoListEntryService(ToDoListEntryRepo toDoListEntryRepo) {
+		super();
+		this.toDoListEntryRepo = toDoListEntryRepo;
+	}
+	
+	
 }
