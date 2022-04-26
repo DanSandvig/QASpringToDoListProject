ad46ad876ad147c16e1a13367d16845746569279
DanSandvig <dspublicaddress@protonmail.com>
2022-04-26 09:43:50 +0100

    Generated getters, setters, hash, equals, and toString

--- src/main/java/com/bae/qaspringtodolistproject/domain/ToDoListEntry.java
+++ src/main/java/com/bae/qaspringtodolistproject/domain/ToDoListEntry.java
@@ -1,5 +1,7 @@
 package com.bae.qaspringtodolistproject.domain;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -48,5 +50,69 @@ public class ToDoListEntry {
 		this.title = title;
 		this.description = description;
 		this.complete = complete;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isComplete() {
+		return complete;
+	}
+
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(complete, description, id, priority, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ToDoListEntry other = (ToDoListEntry) obj;
+		return complete == other.complete && Objects.equals(description, other.description) && id == other.id
+				&& priority == other.priority && Objects.equals(title, other.title);
+	}
+
+	@Override
+	public String toString() {
+		return "ToDoListEntry [id=" + id + ", priority=" + priority + ", title=" + title + ", description="
+				+ description + ", complete=" + complete + "]";
 	}
 }
