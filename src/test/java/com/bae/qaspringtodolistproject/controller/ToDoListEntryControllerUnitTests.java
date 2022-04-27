f28bf0a8ec693f881be24d8a826a15fc9ae767f3
DanSandvig <dspublicaddress@protonmail.com>
2022-04-27 09:11:03 +0100

    Added annotations to class and created private variables

--- src/test/java/com/bae/qaspringtodolistproject/controller/ToDoListEntryControllerUnitTests.java
+++ src/test/java/com/bae/qaspringtodolistproject/controller/ToDoListEntryControllerUnitTests.java
@@ -1,5 +1,25 @@
 package com.bae.qaspringtodolistproject.controller;
 
-public class ToDoListEntryControllerUnitTests {
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
+import com.bae.qaspringtodolistproject.service.ToDoListEntryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+public class ToDoListEntryControllerUnitTests {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
+	@MockBean
+	private ToDoListEntryService toDoListEntryService;
 }
