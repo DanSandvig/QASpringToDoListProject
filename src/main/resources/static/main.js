b048f15180d47169a122ac8e9e2b91da15ae1b76
DanSandvig <dspublicaddress@protonmail.com>
2022-04-27 19:48:35 +0100

    Successful post request with axios. Praise Jordan!

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,9 +1,24 @@
 "use strict"
 
+const baseURL = "http://localhost:8080/todolist";
 const NEW_TODO_FORM = document.getElementById("newtodo");
 
-NEW_TODO_FORM.addEventListener("submit", (e) => {
-    e.preventDefault();
+const addNewToDo = () => {
+    //Creates data from current form values
+    const data = {
+        priority: NEW_TODO_FORM.priority.value,
+        title: NEW_TODO_FORM.title.value,
+        description: NEW_TODO_FORM.description.value,
+        complete: false
+    }
 
-    console.log("Hey There!");
+    //Submits to api using axios post request
+    axios.post(`${baseURL}/create`, data)
+        .then((res) => {console.log(res);})
+        .catch((err) => {console.log(err);});
+}
+
+NEW_TODO_FORM.addEventListener("submit", (e) => {
+    e.preventDefault();     //Prevents normal submission
+    addNewToDo();           //Starts axios post request
 });
