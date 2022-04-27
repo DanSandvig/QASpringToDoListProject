2a96142a4de4773711514f3c000976205c0f85de
DanSandvig <dspublicaddress@protonmail.com>
2022-04-27 09:24:25 +0100

    Created test for controller create method

--- src/test/java/com/bae/qaspringtodolistproject/controller/ToDoListEntryControllerUnitTests.java
+++ src/test/java/com/bae/qaspringtodolistproject/controller/ToDoListEntryControllerUnitTests.java
@@ -1,12 +1,20 @@
 package com.bae.qaspringtodolistproject.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.bae.qaspringtodolistproject.domain.ToDoListEntry;
 import com.bae.qaspringtodolistproject.service.ToDoListEntryService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -22,4 +30,25 @@ public class ToDoListEntryControllerUnitTests {
 	
 	@MockBean
 	private ToDoListEntryService toDoListEntryService;
+	
+	//Methods
+	
+	//Put - Create
+	
+	@Test
+	public void testCreate() throws Exception {
+		ToDoListEntry testInput = new ToDoListEntry(1, "KIS", "Stupid", false);
+		ToDoListEntry mockOutput = new ToDoListEntry(1L, 1, "KIS", "Stupid", false);
+		
+		String testInputAsJson = mapper.writeValueAsString(testInput);
+		String mockOutputAsJson = mapper.writeValueAsString(mockOutput);
+		
+		Mockito.when(toDoListEntryService.create(testInput)).thenReturn(mockOutput);
+		
+		mvc.perform(post("/todolist/create")
+				.content(testInputAsJson)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated())
+				.andExpect(content().json(mockOutputAsJson));
+	}
 }
