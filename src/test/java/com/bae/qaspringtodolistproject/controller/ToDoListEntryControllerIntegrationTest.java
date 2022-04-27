c304098e76a0eb0fed04dcb8c87ce51a8d326e17
DanSandvig <dspublicaddress@protonmail.com>
2022-04-27 10:44:50 +0100

    Added annotations and private variables to integration test

--- src/test/java/com/bae/qaspringtodolistproject/controller/ToDoListEntryControllerIntegrationTest.java
+++ src/test/java/com/bae/qaspringtodolistproject/controller/ToDoListEntryControllerIntegrationTest.java
@@ -1,5 +1,26 @@
 package com.bae.qaspringtodolistproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:testschema.sql", "classpath:testdata.sql"},
+	 executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
 public class ToDoListEntryControllerIntegrationTest {
 
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
 }
