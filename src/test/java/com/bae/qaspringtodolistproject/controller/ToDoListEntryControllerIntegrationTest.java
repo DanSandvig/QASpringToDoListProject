e7e840aa5f16877fef0759e88aee5b2e0baa982f
DanSandvig <dspublicaddress@protonmail.com>
2022-04-27 11:24:20 +0100

    Created integration test for controller create method

--- src/test/java/com/bae/qaspringtodolistproject/controller/ToDoListEntryControllerIntegrationTest.java
+++ src/test/java/com/bae/qaspringtodolistproject/controller/ToDoListEntryControllerIntegrationTest.java
@@ -1,13 +1,20 @@
 package com.bae.qaspringtodolistproject.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.bae.qaspringtodolistproject.domain.ToDoListEntry;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
@@ -22,5 +29,24 @@ public class ToDoListEntryControllerIntegrationTest {
 	
 	@Autowired
 	private ObjectMapper mapper;
+	
+	//Methods
+	
+	//Post - Create
+	
+	@Test
+	public void testCreate() throws Exception {
+		ToDoListEntry testInput = new ToDoListEntry(3, "Again?", "Really?", false);
+		ToDoListEntry mockOutput = new ToDoListEntry(2L, 3, "Again?", "Really?", false);
+		
+		String testInputAsJson = mapper.writeValueAsString(testInput);
+		String mockOutputAsJson = mapper.writeValueAsString(mockOutput);
+		
+		mvc.perform(post("/todolist/create")
+				.content(testInputAsJson)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated())
+				.andExpect(content().json(mockOutputAsJson));
+	}
 	
 }
